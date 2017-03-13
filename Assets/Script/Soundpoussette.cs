3883d2fffd948ef7fda8b2aedeb4ce56d675caee
Mauud <maud.barollet@free.fr>
2017-03-13 22:41:02 +0100

    Ajout du decors bambou

---
+++ Assets/Script/Soundpoussette.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundpoussette : MonoBehaviour {
+
+    public bool positionbois = false;
+    public bool positiongravier = false;
+    public bool positionherbe = false;
+    public Testposition testpositionscript;
+
+    private AudioSource sons;
+    public AudioClip soundbois;
+    public AudioClip soundgravier;
+    public AudioClip soundherbe;
+
+
+    void Update () {
+        // Récupère sur quoi roule le joueur
+        positionbois = testpositionscript.positionbois;
+        positiongravier = testpositionscript.positiongravier;
+        positionherbe = testpositionscript.positionherbe;
+
+        if (positionbois = true)
+            {
+                //sons.PlayOneShot(soundbois);
+            }
+        else if (positionherbe = true)
+            {
+                //sons.PlayOneShot(soundherbe);
+            }
+        else if (positiongravier = true)
+            {
+                //sons.PlayOneShot(soundgravier);
+            }
+    }
+
+}
---
+++ Assets/Script/Testposition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testposition : MonoBehaviour {
+
+    public bool positionbois=false;
+    public bool positionherbe = false;
+    public bool positiongravier = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Est sur du bois
+        if (other.gameObject.name == "box trigger bois")
+        {
+            positionbois = true;
+            positionherbe = false;
+            positiongravier = false;
+        }
+
+        // Est sur de l'herbe ou terre
+        else if (other.gameObject.name == "box trigger herbe")
+        {
+            positionbois = false;
+            positionherbe = true;
+            positiongravier = false;
+        }
+
+        // Est sur du gravier
+        else if (other.gameObject.name == "box trigger gravier")
+        {
+            positionbois = false;
+            positionherbe = false;
+            positiongravier = true;
+        }
+
+    }
+}
